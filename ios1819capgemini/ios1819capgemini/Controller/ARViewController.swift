b54fb1d080f54eeff02ccc670788964cac85cbbe
= <minh.tran@tum.de>
2018-11-28 13:49:42 +0100

    integrate ar into feature branch

--- ios1819capgemini/ios1819capgemini/Controller/ARViewController.swift
+++ ios1819capgemini/ios1819capgemini/Controller/ARViewController.swift
@@ -16,6 +16,14 @@ import Vision
 class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
     // MARK: Stored Instance Properties
+    //for AR
+    var objectAnchorAbsolute = simd_float4(0,0,0,0)
+    var coordinatesPin = [Coordinate(pointX: 0.039081514, pointY: 0.07508006, pointZ: 0.00025102496),
+                          Coordinate(pointX: 0.039081514, pointY: 0.07508006, pointZ: 0.00025102496),
+                          Coordinate(pointX: 0.17503417, pointY: -0.009129599, pointZ: 0.14398605),
+                          Coordinate(pointX: -0.032168947, pointY: 0.023964524, pointZ: 0.011013627),
+                          Coordinate(pointX: -0.29854614, pointY: -0.0061006695, pointZ: -0.070008695)]
+    
     final let scene = SCNScene()
     let ssdPostProcessor = SSDPostProcessor(numAnchors: 1917, numClasses: 2)
     var screenHeight: Double?
@@ -53,12 +61,17 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 //        statusViewController.restartExperienceHandler = { [unowned self] in
 //            self.restartSession()
 //        }
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        sceneView.addGestureRecognizer(gestureRecognizer)
+        
+        configureLighting()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         let config = ARWorldTrackingConfiguration()
+        config.detectionObjects = ARReferenceObject.referenceObjects(inGroupNamed: "TestObjects", bundle: Bundle.main)!
         sceneView.session.run(config)
     }
 
@@ -179,4 +192,120 @@ class ARViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         return 1.0 / (1.0 + exp(-val))
     }
     
+    private func clipToObject (pinReferenceX: Float, pinReferenceY: Float, pinReferenceZ: Float) {
+        coordinatesPin.append( Coordinate(pointX: pinReferenceX - objectAnchorAbsolute.x,
+                                          pointY: pinReferenceY - objectAnchorAbsolute.y,
+                                          pointZ: pinReferenceZ - objectAnchorAbsolute.z))
+        print("relative coordinates:")
+        print(coordinatesPin)
+    }
+    private func loadPin (toPlace: Coordinate, objectAnchor: ARObjectAnchor) -> SCNNode {
+        let planeGeometry = SCNPlane(width: 0.1, height: 0.2)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "pin")
+        planeGeometry.materials = [material]
+        planeGeometry.firstMaterial?.isDoubleSided = true
+        
+        let pinNode = SCNNode(geometry: planeGeometry)
+        pinNode.name = "pin node"
+        pinNode.position = SCNVector3Make(toPlace.pointX + objectAnchor.referenceObject.center.x,
+                                          toPlace.pointY + objectAnchor.referenceObject.center.y,
+                                          toPlace.pointZ + objectAnchor.referenceObject.center.z)
+        
+        sceneView.scene.rootNode.addChildNode(pinNode)
+        print("relative Koordinate: ")
+        print(toPlace.pointX)
+        print(toPlace.pointY)
+        print(toPlace.pointZ)
+        print("Neue absolute Koordinate:")
+        print(toPlace.pointX + objectAnchor.referenceObject.center.x)
+        print(toPlace.pointY + objectAnchor.referenceObject.center.y)
+        print(toPlace.pointZ + objectAnchor.referenceObject.center.z)
+        return pinNode
+    }
+    private func configureLighting() {
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+    }
+    
+    private func add3DPin (x: Float, y: Float, z: Float) {
+        let sphere = SCNSphere(radius: 0.01)
+        let materialSphere = SCNMaterial()
+        materialSphere.diffuse.contents = UIImage(named: "three_notes")
+        sphere.materials = [materialSphere]
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.name = "sphere"
+        sphereNode.position = SCNVector3(x, y, z)
+        sceneView.scene.rootNode.addChildNode(sphereNode)
+    }
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        
+        if recognizer.state == .ended {
+            let location: CGPoint = recognizer.location(in: sceneView)
+            let hits = self.sceneView.hitTest(location, options: nil)
+            if !hits.isEmpty{
+                print("node detected.")
+                let tappedNode = hits.first?.node
+                guard let name = tappedNode?.name else{
+                    print("no name node")
+                    // Get exact position where touch happened on screen of iPhone (2D coordinate)
+                    let touchPosition = recognizer.location(in: sceneView)
+                    
+                    // Conduct a hit test based on a feature point that ARKit detected to find out what 3D point this 2D coordinate relates to
+                    let hitTestResult = sceneView.hitTest(touchPosition, types: .featurePoint)
+                    
+                    if !hitTestResult.isEmpty {
+                        guard let hitResult = hitTestResult.first else {
+                            return
+                        }
+                        
+                        add3DPin(x: hitResult.worldTransform.columns.3.x,
+                                 y: hitResult.worldTransform.columns.3.y,
+                                 z: hitResult.worldTransform.columns.3.z)
+                        
+                        //                         addPin(x: hitResult.worldTransform.columns.3.x,
+                        //                               y: hitResult.worldTransform.columns.3.y,
+                        //                               z: hitResult.worldTransform.columns.3.z)
+                        
+                        print("tap coordinate \(hitResult.worldTransform.columns.3)")
+                        clipToObject(pinReferenceX: hitResult.worldTransform.columns.3.x, pinReferenceY: hitResult.worldTransform.columns.3.y, pinReferenceZ: hitResult.worldTransform.columns.3.z)
+                    }
+                    return
+                }
+                print(name)
+            }
+        }
+    }
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        print("renderer() aufgerufen")
+        
+        let node = SCNNode()
+        
+        if let objectAnchor = anchor as? ARObjectAnchor {
+            
+            objectAnchorAbsolute = objectAnchor.transform.columns.3
+            print("definitely the absolute position of the chair anchor \(objectAnchorAbsolute)")
+            //Maybe we should consider this method for easier computations. sceneView.session.setWorldOrigin(relativeTransform: point)
+            
+            for pin in coordinatesPin {
+                node.addChildNode(loadPin(toPlace: pin, objectAnchor: objectAnchor))
+            }
+            let alert = UIAlertController(title: "Object detected", message: "\(objectAnchor.referenceObject.name ?? "no name")", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            print("detected object anchor absolute position. x: \(objectAnchorAbsolute.x), y: \(objectAnchorAbsolute.y), x: \(objectAnchorAbsolute.z)")
+        }
+        return node
+    }
+}
+
+// MARK: Coordinate
+struct Coordinate {
+    var pointX: Float
+    var pointY: Float
+    var pointZ: Float
+    
+    var description: String {
+        return "x: \(pointX), y: \(pointY), z: \(pointZ) "
+    }
 }
