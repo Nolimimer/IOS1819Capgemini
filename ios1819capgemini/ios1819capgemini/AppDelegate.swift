b41f94bdef0ce962cbe5cbebb5d79d84b4303181
Anna Kovaleva <anna.kovaleva@tum.de>
2018-12-23 16:28:05 +0300

    Add ScannerApp application. Adapt AppDelegate. Change initial VC

--- ios1819capgemini/ios1819capgemini/AppDelegate.swift
+++ ios1819capgemini/ios1819capgemini/AppDelegate.swift
@@ -43,6 +43,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, IKAppDelegate {
 
     func applicationWillResignActive(_ application: UIApplication) {
         DataHandler.saveToJSON()
+        if let viewController = self.window?.rootViewController as? ViewController {
+            viewController.blurView?.isHidden = false
+        }
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
@@ -54,10 +57,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate, IKAppDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
+        if let viewController = self.window?.rootViewController as? ViewController {
+            viewController.backFromBackground()
+        }
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
+        if let viewController = self.window?.rootViewController as? ViewController {
+            viewController.blurView?.isHidden = true
+        }   
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
