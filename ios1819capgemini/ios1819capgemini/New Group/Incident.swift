7da6360985d996b83f49e5f1a0c8694063704a63
= <minh.tran@tum.de>
2019-01-11 16:23:47 +0100

    remove pin colouring from incident class

--- ios1819capgemini/ios1819capgemini/New Group/Incident.swift
+++ ios1819capgemini/ios1819capgemini/New Group/Incident.swift
@@ -39,13 +39,6 @@ public class Incident: Codable {
         self.status = status
         self.description = description
         self.modifiedDate = modifiedDate
-        
-        switch status {
-        case .open: self.changePinColor(to: UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.9))
-        case .progress: self.changePinColor(to: UIColor(red: 239.0/255.0, green: 196.0/255.0, blue: 0.0, alpha: 0.9))
-        case .resolved: self.changePinColor(to: UIColor(red: 22.0/255.0, green: 167.0/255.0, blue: 0.0, alpha: 0.9))
-        }
-        
     }
     
     func editIncidentType(type: IncidentType) {
@@ -56,20 +49,7 @@ public class Incident: Codable {
     private func suggest() -> IncidentType? {
         return nil
     }
-    
-    private func changePinColor(to color: UIColor) {
-        for node in nodes {
-            guard let name = node.name,
-                let nodeId = Int(name) else {
-                    print("no node found")
-                    return
-            }
-            if nodeId == identifier {
-                node.geometry?.materials.first?.diffuse.contents = color
-            }
-        }
-    }
-    
+
     func getCoordinateToVector() -> SCNVector3 {
         var res = SCNVector3.init()
         res.x = coordinate.pointX
